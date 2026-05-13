47891b1f21fb3a086a62bcec05d577fd3b6e5c4d
zandimt <andimtoader@gmail.com>
2026-05-13 10:13:57 +0200

    fixed membership url

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const MEMBERSHIP_FORM_URL = 'https://forms.gle/example-gears-membership'
+export const MEMBERSHIP_FORM_URL = 'https://docs.google.com/forms/d/e/1FAIpQLScOTrRwjyibTXeyARh09pIGIhvpeOx1ly8fGXQ6feubCfRUOQ/viewform?usp=publish-editor'
 export const EVENT_INTEREST_FORM_URL = 'https://forms.gle/example-gears-events'
 
