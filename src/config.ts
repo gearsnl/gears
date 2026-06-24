74bbd691019d815f8b77a31bbbcba42f0fbefb50
zandimt <andimtoader@gmail.com>
2026-06-24 12:39:11 +0200

    new form

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const MEMBERSHIP_FORM_URL = 'https://docs.google.com/forms/d/e/1FAIpQLScOTrRwjyibTXeyARh09pIGIhvpeOx1ly8fGXQ6feubCfRUOQ/viewform?usp=publish-editor'
+export const MEMBERSHIP_FORM_URL = 'https://forms.gle/pQjcvoZKfumQNjtk6'
 export const EVENT_INTEREST_FORM_URL = 'https://forms.gle/example-gears-events'
 
