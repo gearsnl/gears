e4fe1560c37f0c4b5d649d4043bf475bdddbbf82
zandi t. <69138013+zandimt@users.noreply.github.com>
2026-07-30 14:41:08 +0200

    Fix typo in SectionLogin component

--- src/components/SectionLogin.tsx
+++ src/components/SectionLogin.tsx
@@ -5,7 +5,7 @@ export function SectionLogin() {
         <h2>Contact &amp; partners</h2>
         <p>
           Interested in collaborating, sponsoring hardware or inviting us for a
-          demo? Reach out to the GEARS boardd.
+          demo? Reach out to the GEARS board.
         </p>
       </div>
 
